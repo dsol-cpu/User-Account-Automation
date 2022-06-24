8f2293a71f747c7908208976e1d88f3c197295a3
dsol_cpu <dmsolinsky@email.wm.edu>
2022-06-24 00:08:44 -0400

    add user agent

--- tiktok-bot.py
+++ tiktok-bot.py
@@ -3,6 +3,8 @@ import datetime
 import time
 import random
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -18,16 +20,16 @@ class My_Chrome():
 	def __init__(self, jsondata, csvdata=[], zoho=False):
 
 		self.csvdata = csvdata
-		self.firstName = jsondata["firstName"]
-		self.lastName = jsondata["lastName"]
 		self.email = jsondata["mainEmail"]
 		self.password = jsondata["password"]
 		self.logIn = False
 
 		#TODO: turn off the SameSite cookie for lastmx
-
-		self.browser = uc.Chrome()
+		opts = Options()
+		opts.add_argument("user-agent=Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41")
+		self.browser = uc.Chrome(options = opts)
 		self.browser.delete_all_cookies()
+		
 		self.zoho = zoho
 
 	def saveSuccessfulInfo(self, successList):
@@ -45,7 +47,10 @@ class My_Chrome():
 		self.browser.get("https://www.tiktok.com/signup/phone-or-email/email")
 		self.browser.implicitly_wait(10)
 		time.sleep(2)
-		
+
+		#csrf token where
+
+		# input("Hanging")
 		email = self.csvdata[0]
 
 		#TODO: make random password generation
@@ -105,23 +110,32 @@ class My_Chrome():
 				print("success!")
 			except Exception as e: 
 				print("error!")
+				self.browser.refresh()
+				time.sleep(1)
 				pass
 
 		self.browser.find_element(By.XPATH, '//button[text() = "Send code"]').click()
 
 		# Get the code
-		tikTokCode = self.getRecentCode(passw)
-		print("found code: " + tikTokCode)
+		tikTokCode = self.getRecentCode()
+		print("found code: " + str(tikTokCode))
 
-		self.browser.close()
+		#self.browser.close()
 		self.browser.switch_to.window(self.browser.window_handles[0])
-		self.browser.find_elements(By.XPATH, '//input[@type = "text"]')[1].send_keys(tikTokCode)
+		time.sleep(5)
+		self.browser.find_element(By.XPATH, '//*[@placeholder = "Enter 6-digit code"]').send_keys(tikTokCode)
 		self.browser.find_element(By.CSS_SELECTOR, "button[type = 'submit']").click()
+
+		qrcode = WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.XPATH, '//img')))
+		if(qrcode):
+			print("placeholder scan")
+			
+		
 
 		# self.saveSuccessfulInfo({email, passw})
  
 	#Google Signin
-	def googleSignIn(self, passw):
+	def googleSignIn(self):
 		if self.zoho:
 			self.zohoSignIn()
 			zohoOTPCode = self.zohoSignIn()
@@ -135,6 +149,10 @@ class My_Chrome():
 		# #proceed with new tab
 		# self.browser.switch_to.window(self.browser.window_handles[1])
 		
+		self.browser.execute_script("window.open('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin');") 
+		#proceed with new tab
+		self.browser.switch_to.window(self.browser.window_handles[1])
+
 		WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id="identifierId"]')))
 		gmailUserBox = self.browser.find_element(By.XPATH, '//input[@id="identifierId"]')
 		gmailUserBox.send_keys(self.email)
@@ -143,8 +161,42 @@ class My_Chrome():
 		WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.ID, 'identifierId')))
 		gmailPasswBox = self.browser.find_element(By.NAME, 'password')
 		gmailPasswBox.send_keys(self.password)
+		time.sleep(1)
 		gmailPasswBox.send_keys(Keys.ENTER)
 		print("signed into google!")
+
+		print("Please wait a minute before the code is sent to your email")
+		
+		time.sleep(1)
+
+		gmails = self.browser.find_elements(By.XPATH, '//*[@class="bog"]')
+		while(True):
+			for gmail in gmails:
+				if "verification code" in gmail.text:
+					print(gmail.text[:6])
+					return str(gmail.text[:6])
+			print("No code found!")
+			time.sleep(5)
+			self.browser.refresh()
+		
+	def yahooSignIn(self):
+		self.browser.execute_script("window.open('https://login.yahoo.com/?.intl=us&.lang=en-US&src=ym&activity=mail-direct&pspid=159600001&done=https%3A%2F%2Fmail.yahoo.com%2Fd&add=1');") 
+		#proceed with new tab
+		self.browser.switch_to.window(self.browser.window_handles[1])
+
+		WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@name="username"]')))
+		gmailUserBox = self.browser.find_element(By.XPATH, '//input[@name="username"]')
+		gmailUserBox.send_keys(self.email)
+		gmailUserBox.send_keys(Keys.ENTER)
+
+		WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id = "login-passwd"]')))
+		gmailPasswBox = self.browser.find_element(By.XPATH, '//input[@id = "login-passwd"]')
+		gmailPasswBox.send_keys(self.password)
+		time.sleep(1)
+		gmailPasswBox.send_keys(Keys.ENTER)
+		print("signed into google!")
+
+		pass
 
 	#Zoho Signin
 	def zohoSignIn(self, email):
@@ -157,22 +209,11 @@ class My_Chrome():
 		self.browser.find_element(By.ID, 'nextbtn').click()   
 
 	#Wait for Google Signin and grab code from email
-	def getRecentCode(self, passw):
-		self.browser.execute_script("window.open('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin');") 
-		#proceed with new tab
-		self.browser.switch_to.window(self.browser.window_handles[1])
-
+	def getRecentCode(self):
+		# stalling = input("stalling before google signin")
 		if not self.logIn:
-			self.googleSignIn(passw)
+			return self.googleSignIn()
 
-		print("Please wait a minute before the code is sent to your email")
-		# time.sleep(5)
-		# self.browser.refresh()
-
-		gmails = self.browser.find_elements(By.XPATH, '//span[@class="bog"]')
-		for gmail in gmails:
-			if "verification code" in gmail.text:
-				return gmail.text[:6]
 
 	def main(self):
 		# self.browser.maximize_window()
@@ -186,7 +227,7 @@ if __name__ == "__main__":
 	data.close()
 	csvdata = []
 
-	with open(os.getcwd() + '/emailList.csv', mode ='r') as file:
+	with open(os.getcwd() + '/emails.csv', mode ='r') as file:
 		# reading the CSV file
 		csvFile = csv.reader(file)
 
